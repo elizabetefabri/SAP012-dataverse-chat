be1c1485c686ff81bd679cab5299b647f5bbdaac
Elizabete <elizabete958@gmail.com>
2024-02-25 21:43:49 -0300

    Alter-> description para shortDescription e responsivo ok

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,17 +4,16 @@ export const Card = (data) => {
   ul.id = "cards";
 
   data.forEach((item) => {
-    const description = window.innerWidth <= 768 ? item.shortDescription : (window.innerWidth > 768 ? item.description : '');
 
     ul.innerHTML += `
       <li itemscope itemtype="OsMelhoresFilmes" class="container__card">
         <div class="content__card">
-          <a href="" class="link__card">
+          <a href="chat?id=${item.id}" class="link__card">
             <dl itemscope itemtype="#">
               <dt><img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card" /></dt>
               <dd itemprop="name" class="name__card">${item.name}</dd>
               <dd itemprop="idadePersona" class="idade__card">Sou ${item.facts.paisNascimentoPersona}, com ${item.facts.idadePersona} anos</dd>
-              <dd itemprop="description" class="description__card">${description}</dd>
+              <dd itemprop="shortDescription" class="description__card">${item.shortDescription}</dd>
             </dl>
           </a>
         </div>

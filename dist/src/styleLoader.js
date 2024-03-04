1130eb86f901c91c8562776d7a48734e71424829
Elizabete <elizabete958@gmail.com>
2024-03-04 12:31:22 -0300

    alter icon

--- dist/src/styleLoader.js
+++
@@ -1,8 +0,0 @@
-const loadStyle = (stylePath) => {
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = stylePath;
-  document.querySelector('head').appendChild(link);
-};
-
-export default loadStyle;

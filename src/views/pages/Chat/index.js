786d976c9e48a396b6cd22c104714544f47cd39b
Elizabete <elizabete958@gmail.com>
2024-03-05 19:30:24 -0300

    otimização do codigo

--- src/views/pages/Chat/index.js
+++ src/views/pages/Chat/index.js
@@ -4,107 +4,125 @@ import loadStyle from "../../../styleLoader.js";
 import { communicateWithOpenAI } from "../../../lib/openAIApi.js";
 
 export const Chat = ({ id }) => {
+  // Carregar estilo CSS
   loadStyle("./views/pages/Chat/style.css");
 
-  const users = data.find((user) => user.id === parseInt(id));
-  const viewEl = document.createElement("div");
-  viewEl.classList.add("background");
+  // Encontrar informações do usuário
+  const user = data.find((user) => user.id === parseInt(id));
 
-  const personaDescriptionToChat = `Você é um: ${users.name}.${users.shortDescription}`;
+  // Criar elemento de visualização
+  const viewEl = createViewElement(user);
 
-  const headerData = {
-    img: {
-      src: `${users.imageUrlChat}`,
-      alt: "Image users",
-    },
+  // Inserir cabeçalho antes do elemento de visualização
+  insertHeaderBefore(viewEl);
+
+  // Elementos DOM
+  const inputChat = viewEl.querySelector("#input__chat");
+  const btnEnviarChat = viewEl.querySelector("#btn_enviar_chat");
+  const messagesContainer = viewEl.querySelector("#messages");
+  const openaiDigitando = viewEl.querySelector("#openai_digitando");
+
+  // Histórico de conversas
+  const conversationHistory = [
+    { role: "system", content: `Você é um ${user.name}: ${user.shortDescription}` },
+  ];
+
+  // Atualizar chat com nova mensagem
+  const updateChat = (message) => {
+    messagesContainer.insertAdjacentHTML('beforeend', `<div class="${message.role}-message">${message.content}</div>`);
   };
 
+  // Adicionar mensagem ao histórico de conversas
+  const addMessageToHistory = (role, content) => {
+    conversationHistory.push({ role, content });
+  };
+
+  // Enviar mensagem do usuário para a OpenAI e atualizar o chat
+  const sendUserMessageToOpenAI = async (message) => {
+    addMessageToHistory("user", message);
+    updateChat({ role: "user", content: message });
+    openaiDigitando.textContent = `${user.name} está digitando...`;
+    await sendOpenAIMessage();
+    openaiDigitando.textContent = "";
+    inputChat.value = "";
+  };
+
+  // Enviar mensagem para a OpenAI e atualizar o chat
+  const sendOpenAIMessage = async () => {
+    const response = await communicateWithOpenAI(conversationHistory);
+    updateChat({ role: "openai", content: `<img src="../../../images/chat/icon-chat.png" alt="image" style="width: 30px; height: 30px;"> ${response.content}` });
+  };
+
+  // Lidar com envio de mensagem pelo usuário
+  const handleUserMessage = async () => {
+    const message = inputChat.value.trim();
+    if (!message) return;
+    await sendUserMessageToOpenAI(message);
+  };
+
+  // Adicionar evento de clique para o botão de enviar mensagem
+  btnEnviarChat.addEventListener("click", handleUserMessage);
+
+  // Adicionar evento de pressionar a tecla "Enter" para o input de chat
+  inputChat.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      handleUserMessage();
+      inputChat.value = "";
+    }
+  });
+
+  return viewEl;
+};
+
+// Criar elemento de visualização
+const createViewElement = (user) => {
+  const viewEl = document.createElement("div");
+  viewEl.classList.add("background");
   viewEl.innerHTML = `
     <main class="container__main">
       <div class="content-main">
         <section class="chat-content individual">
           <div class="container__header">
-            <img src="${users.imageUrlChat}" alt="image" style="width: 50px; height: 50px;">
+            <img src="${user.imageUrlChat}" alt="image" style="width: 50px; height: 50px;">
             <div class="container__name">
-              <h3>${users.name}</h3>
-              <p>${users.quote}</p>
+              <h3>${user.name}</h3>
+              <p>${user.quote}</p>
             </div>
             <div id="messageError" class="error-message hide"></div>
           </div>
         </section>
 
         <section id="chat">
-            <div id="messages">
-            </div>
-          </section>
-          <div id="openai_digitando"></div>
-          <section class="container__input relative">
-            <div class="container_input__chat">
-              <input class="input__chat" id="input__chat" type="text" placeholder="Digite sua pergunta?" />
-              <button id="btn_enviar_chat">
-                <i class='fas fa-location-arrow fa-lg'></i>
-              </button>
-            </div>
-          </section>
+          <div id="messages"></div>
+        </section>
+        <div id="openai_digitando"></div>
+        <section class="container__input relative">
+          <div class="container_input__chat">
+            <input class="input__chat" id="input__chat" type="text" placeholder="Digite sua pergunta?" />
+            <button id="btn_enviar_chat">
+              Enviar
+              <i class='fas fa-location-arrow fa-lg'></i>
+            </button>
+          </div>
+        </section>
       </div>
     </main>
   `;
-  document.body.insertAdjacentHTML("beforebegin", `
-  <header>
-    ${Header(headerData).outerHTML}
-  </header>
-`);
-
-  const input__chat = viewEl.querySelector("#input__chat");
-  const btn__enviar_chat = viewEl.querySelector("#btn_enviar_chat");
-  const openai_digitando = viewEl.querySelector("#openai_digitando");
-  const messages = viewEl.querySelector("#messages");
-  const messageError = viewEl.querySelector("#messageError");
-
-  const conversationHistory = [
-    { role: "system", content: `Você é um ${personaDescriptionToChat}` },
-  ];
-
-  const updateChat = (message) => {
-    messages.innerHTML += `<div class="${message.role}-message">${message.content}</div>`;
-  };
-
-  input__chat.addEventListener("keypress", async (event) => {
-    if (event.key === "Enter") {
-      sendMessage();
-    }
-  });
-
-  btn__enviar_chat.addEventListener("click", sendMessage);
-
-  async function sendMessage() {
-    const mensagemEnviada = input__chat.value;
-    if (!mensagemEnviada.trim()) {
-      return;
-    }
-
-    // updateChat({ role: "user", content: mensagemEnviada });
-    updateChat({ role: "user", content: `<i class='fas fa-user-secret fa-lg'></i> ${mensagemEnviada}` });
-    input__chat.value = "";
-
-    openai_digitando.innerHTML = `${users.name} está digitando...`;
-
-    try {
-      // Adicionando a última mensagem do usuário à conversa antes de enviar para a API do OpenAI
-      conversationHistory.push({ role: "user", content: mensagemEnviada });
-      const resposta = await communicateWithOpenAI(conversationHistory);
-      // updateChat({ role: "openai", content: resposta.content });
-      updateChat({ role: "openai", content: `<i class='fas fa-robot fa-lg'></i> ${resposta.content}` });
-
-      openai_digitando.innerHTML = "";
-    } catch (error) {
-      openai_digitando.innerHTML = "";
-      messageError.classList.add("pop-up");
-      messageError.innerHTML = error.message;
-      console.error("Erro ao se comunicar com a OpenAI", error);
-    }
-  }
-
   return viewEl;
 };
 
+// Inserir cabeçalho antes do elemento de visualização
+const insertHeaderBefore = (viewEl) => {
+  const headerData = {
+    img: {
+      src: viewEl.querySelector("img").getAttribute("src"),
+      alt: "Image users",
+    },
+  };
+
+  document.body.insertAdjacentHTML("beforebegin", `
+    <header>
+      ${Header(headerData).outerHTML}
+    </header>
+  `);
+};
